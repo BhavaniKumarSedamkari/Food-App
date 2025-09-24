674d46d4207a3c3e3847c4c6745f9a489f76fd5d
Sedamkari bhavani Kumar <2000078145@hexaware.com>
2025-09-24 19:30:29 +0530

    updated cors

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,8 +19,8 @@ const Body=()=>{
     },[]);
 
     const callingApi = async ()=>{
-
-    const apicall = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    //    https://corsproxy.io/?url= 
+    const apicall = await fetch("https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json = await apicall.json();
     setresobjStatevarible(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     setfilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,4 +4,4 @@ export var headerLogo=
 export var restroImage=
 "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export var api_menu="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId="
+export var api_menu="https://proxy.corsfix.com/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId="
